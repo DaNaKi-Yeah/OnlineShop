a8165fdbe6a2875f1daddcf8f6bf0a1b0a5b3326
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-05-18 21:15:15 +0600

    Application: add interface IOnlineShopDbContext

--- OnlineShop.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OnlineShop.Application
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ OnlineShop.Application/Interfaces/IOnlineShopDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Application.Interfaces
+{
+    public interface IOnlineShopDbContext
+    {
+        DbSet<BuyItem> BuyItems { get; set; }
+        DbSet<Cart> Carts { get; set; }
+        DbSet<Category> Categories { get; set; }
+        DbSet<Client> Clients { get; set; }
+        DbSet<Order> Orders { get; set; }
+        DbSet<Payment> Payments { get; set; }
+        DbSet<Product> Products { get; set; }
+        DbSet<ProductPropertyValuesInventory> ProductPropertyValuesInventories { get; set; }
+        DbSet<Property> PropertyValues { get; set; }
+        DbSet<Review> Reviews { get; set; }
+        DbSet<Value> Values { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
