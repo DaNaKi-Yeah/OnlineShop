a6f51e519c328ecccdc1c09ddb7392fc064af91e
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-05-22 18:06:12 +0600

    Application: add class BaseHandler, Forlder CQRS with folder Property, add class PropertyHandler, CreatePropertyCommand(+Handler), RemoveByIdPropertyCommand(+Handler), GetPropertyQuery(+Handler), SearchPropertiesQuery(+Handler).

---
+++ OnlineShop.Application/CQRS/Properties/Queries/GetProperty/GetPropertyQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using OnlineShop.Domain.Models;
+
+namespace OnlineShop.Application.CQRS.Properties.Queries.GetProperty
+{
+    public class GetPropertyQuery : IRequest<Property>
+    {
+        public int Id { get; set; }
+    }
+}
