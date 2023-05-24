f73d47d642449ea2825fb03e9598496609c32a96
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-05-24 18:17:40 +0600

    Api: add SQLServerConnectionString in appsetting.json, register dependencies

--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -1,6 +1,31 @@
+using OnlineShop.Application;
+using OnlineShop.Application.Common.Mappings;
+using OnlineShop.Application.Interfaces;
+using OnlineShop.Persistence;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(IOnlineShopDbContext).Assembly));
+});
+
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +42,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
