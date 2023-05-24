a2057ed103633d450dda8c1fc923a2658efff082
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-05-24 18:21:51 +0600

    Api: add BaseController

---
+++ OnlineShop/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Application.Repositories.Interfaces;
+
+namespace OnlineShop.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class BaseController: ControllerBase
+    {
+        protected readonly IMediator _mediator;
+        protected readonly IMapper _mapper;
+
+        public BaseController(IMediator mediator, IMapper mapper) { _mediator = mediator; _mapper = mapper;}
+    }
+}
