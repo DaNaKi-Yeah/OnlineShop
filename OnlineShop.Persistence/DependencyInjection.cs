f239466be30d4856167ca3e3fc42fa90ebbb4683
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-05-24 18:14:07 +0600

    Persistence: add class DependencyInjection

---
+++ OnlineShop.Persistence/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineShop.Application.Interfaces;
+using OnlineShop.Persistence.Db.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration) 
+        {
+            string connectionString = configuration.GetConnectionString("SQLServerConnectionString");
+            services.AddDbContext<SQLServerOnlineShopDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddScoped(typeof(IOnlineShopDbContext), typeof(SQLServerOnlineShopDbContext));
+
+            return services;
+        }
+    }
+}
