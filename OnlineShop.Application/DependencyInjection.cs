b24de0c5f0ea2fe4fd5505fbc2ebbccdf984212f
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-05-24 18:13:13 +0600

    Application: add class DependencyInjection

---
+++ OnlineShop.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OnlineShop.Application.Repositories.Implementations;
+using OnlineShop.Application.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(configuration => configuration.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+            services.AddScoped(typeof(IRepository<,>), typeof(BaseRepository<,>));
+
+            return services;
+        }
+    }
+}
