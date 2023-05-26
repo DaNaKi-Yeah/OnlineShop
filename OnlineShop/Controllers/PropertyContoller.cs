0753134e410589f2b189efe1b3d2c58a9e7dc346
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-05-26 19:03:40 +0600

    Api: add PropertyController, ValueController

---
+++ OnlineShop/Controllers/PropertyContoller.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Application.CQRS.Properties.Commands.CreateProperty;
+using OnlineShop.Application.CQRS.Properties.Commands.RemoveProperty;
+using OnlineShop.Application.CQRS.Properties.DTOs;
+using OnlineShop.Application.CQRS.Properties.Queries.GetProperty;
+using OnlineShop.Application.CQRS.Properties.Queries.SearchProperties;
+using OnlineShop.Application.Repositories.Interfaces;
+using OnlineShop.Domain.Models;
+
+namespace OnlineShop.API.Controllers
+{
+    public class PropertyController : BaseController
+    {
+        public PropertyController(IMediator mediator, IMapper mapper) : base(mediator, mapper) { }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<int> Create([FromBody] CreatePropertyCommand command)
+        {
+            var id = await _mediator.Send(command);
+
+            return id;
+        }
+
+        [HttpDelete]
+        [Route("Remove")]
+        public async Task Remove([FromQuery] RemoveByIdPropertyCommand command)
+        {
+            await _mediator.Send(command);
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        public async Task<GetPropertyDTO> GetById([FromQuery] GetPropertyByIdQuery command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        [HttpGet]
+        [Route("Search")]
+        public async Task<List<GetPropertyDTO>> Search([FromQuery] SearchPropertiesQuery query)
+        {
+            return await _mediator.Send(query);
+        }
+    }
+}
---
+++ OnlineShop/Controllers/ValueController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Application.CQRS.Values.Commands.CreateValue;
+using OnlineShop.Application.CQRS.Values.Commands.RemoveByIdValue;
+using OnlineShop.Application.CQRS.Values.DTOs;
+using OnlineShop.Application.CQRS.Values.Queries.GetValue;
+using OnlineShop.Application.CQRS.Values.Queries.SearchValues;
+
+namespace OnlineShop.API.Controllers
+{
+    public class ValueController : BaseController
+    {
+        public ValueController(IMediator mediator, IMapper mapper) : base(mediator, mapper) { }
+
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<int> Create([FromBody] CreateValueCommand command)
+        {
+            var id = await _mediator.Send(command);
+
+            return id;
+        }
+
+        [HttpDelete]
+        [Route("Remove")]
+        public async Task Remove([FromQuery] RemoveByIdValueCommand command)
+        {
+            await _mediator.Send(command);
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        public async Task<GetValueDTO> GetById([FromQuery] GetValueByIdQuery query)
+        {
+            return await _mediator.Send(query);
+        }
+
+        [HttpGet]
+        [Route("Search")]
+        public async Task<List<GetValueDTO>> Search([FromQuery] SearchValueQuery query)
+        {
+            return await _mediator.Send(query);
+        }
+    }
+}
