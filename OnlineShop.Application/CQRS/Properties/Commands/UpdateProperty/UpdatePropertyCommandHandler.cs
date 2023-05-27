c5c3fc4297c601e3d60605fb62705a7f591bec5c
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-05-27 14:42:58 +0600

    Application: add UpdateProperty, UpdateValue

---
+++ OnlineShop.Application/CQRS/Properties/Commands/UpdateProperty/UpdatePropertyCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using OnlineShop.Application.CQRS.Properties.Handlers;
+using OnlineShop.Application.Repositories.Interfaces;
+using OnlineShop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Application.CQRS.Properties.Commands.UpdateProperty
+{
+    public class UpdatePropertyCommandHandler : PropertyHandler, IRequestHandler<UpdatePropertyCommand>
+    {
+        public UpdatePropertyCommandHandler(IRepository<Property, int> repository, IMapper mapper) : base(repository, mapper) { }
+
+        public async Task Handle(UpdatePropertyCommand request, CancellationToken cancellationToken)
+        {
+            var property = await _repository.GetByIdAsync(request.Id);
+            property.Name = request.Name;
+
+            await _repository.UpdateAsync(property);
+        }
+    }
+}
---
+++ OnlineShop.Application/CQRS/Values/Commands/UpdateValue/UpdateValueCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Application.CQRS.Values.Commands.UpdateValue
+{
+    public class UpdateValueCommand : IRequest
+    {
+        [Required]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
