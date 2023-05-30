28b9c960624db3a99a85f39c07e7dea31ee1cd1f
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-05-30 18:28:46 +0600

    Application: update GetProductDTO, CreateProductCommand

--- OnlineShop.Application/CQRS/Products/Commands/CreateProduct/CreateProductCommand.cs
+++ OnlineShop.Application/CQRS/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
 
 using OnlineShop.Application.Common.Mappings;
 using OnlineShop.Domain.Models;
@@ -14,6 +15,17 @@ namespace OnlineShop.Application.CQRS.Properties.Commands.CreateProperty
         public string PictureLink { get; set; }
         public decimal Price { get; set; }
         [Required]
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreateProductCommand, Product>()
+                .ForMember(x => x.ModelName, opt => opt.MapFrom(y => y.ModelName))
+                .ForMember(x => x.YearOfProduction, opt => opt.MapFrom(y => y.YearOfProduction))
+                .ForMember(x => x.Description, opt => opt.MapFrom(y => y.Description))
+                .ForMember(x => x.PictureLink, opt => opt.MapFrom(y => y.PictureLink))
+                .ForMember(x => x.Price, opt => opt.MapFrom(y => y.Price))
+                .ForMember(x => x.CategoryId, opt => opt.MapFrom(y => y.CategoryId));
+        }
     }
 }
--- OnlineShop.Application/CQRS/Products/DTOs/GetProductDTO.cs
+++ OnlineShop.Application/CQRS/Products/DTOs/GetProductDTO.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 
 using OnlineShop.Application.Common.Mappings;
+using OnlineShop.Application.CQRS.Properties.Commands.CreateProperty;
 using OnlineShop.Domain.Models;
 
 namespace OnlineShop.Application.CQRS.Products.DTOs
@@ -13,5 +14,17 @@ namespace OnlineShop.Application.CQRS.Products.DTOs
         public string Description { get; set; }
         public string PictureLink { get; set; }
         public decimal Price { get; set; }
+        public string CategoryName { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Product, GetProductDTO>()
+                .ForMember(x => x.ModelName, opt => opt.MapFrom(y => y.ModelName))
+                .ForMember(x => x.YearOfProduction, opt => opt.MapFrom(y => y.YearOfProduction))
+                .ForMember(x => x.Description, opt => opt.MapFrom(y => y.Description))
+                .ForMember(x => x.PictureLink, opt => opt.MapFrom(y => y.PictureLink))
+                .ForMember(x => x.Price, opt => opt.MapFrom(y => y.Price))
+                .ForMember(x => x.CategoryName, opt => opt.MapFrom(y => y.Category.Name));
+        }
     }
 }
