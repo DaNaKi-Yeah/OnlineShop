f0323dbd8b5cd03c1c38198a87578eac0f3cb8a1
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-05-31 19:01:03 +0600

    Api: add CategoryPropertyController

---
+++ OnlineShop/Controllers/CategoryPropertyController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Application.CQRS.CategoryProperties.Commands.CreateCategoryProperty;
+using OnlineShop.Application.CQRS.CategoryProperties.Commands.RemoveByIdCategoryProperty;
+using OnlineShop.Application.CQRS.CategoryProperties.DTOs;
+using OnlineShop.Application.CQRS.CategoryProperties.Queries.GetCategoryPropertyById;
+using OnlineShop.Application.CQRS.CategoryProperties.Queries.SearchCategoryProperties;
+
+namespace OnlineShop.API.Controllers
+{
+    public class CategoryPropertyController : BaseController
+    {
+        public CategoryPropertyController(IMediator mediator, IMapper mapper) : base(mediator, mapper) { }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<GetCategoryPropertyDTO> Create([FromBody] CreateCategoryPropertyCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            return result;
+        }
+
+        [HttpDelete]
+        [Route("RemoveById")]
+        public async Task Remove([FromQuery] RemoveByIdCategoryPropertyCommand command)
+        {
+            await _mediator.Send(command);
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        public async Task<GetCategoryPropertyDTO> GetById([FromQuery] GetCategoryPropertyByIdQuery query)
+        {
+            var result = await _mediator.Send(query);
+
+            return result;
+        }
+
+        [HttpGet]
+        [Route("Search")]
+        public async Task<List<GetCategoryPropertyDTO>> Search([FromQuery] SearchCategoryPropertiesQuery query)
+        {
+            var result = await _mediator.Send(query);
+
+            return result;
+        }
+    }
+}
