57496d3999f619cd1cc9441ca577434a2621ca87
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-06-01 18:54:08 +0600

    Application: add GetPropertyValuesByCategoryId

---
+++ OnlineShop.Application/CQRS/PropertyValues/Queries/GetPropertyValuesByCategoryId/GetPropertyValuesByCategoryIdHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Application.CQRS.PropertyValues.DTOs;
+using OnlineShop.Application.CQRS.PropertyValues.Handlers;
+using OnlineShop.Application.Repositories.Interfaces;
+using OnlineShop.Domain.Relations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Application.CQRS.PropertyValues.Queries.GetPropertyValuesByCategoryId
+{
+    public class GetPropertyValuesByCategoryIdHandler : PropertyValueHandler, IRequestHandler<GetPropertyValuesByCategoryIdQuery, List<GetPropertyValueDTO>>
+    {
+        public GetPropertyValuesByCategoryIdHandler(IRepository<PropertyValue, int> repository, IMapper mapper) : base(repository, mapper) { }
+
+        public async Task<List<GetPropertyValueDTO>> Handle(GetPropertyValuesByCategoryIdQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _repository.GetQuery().Where(x => 
+                x.Property.CategoryProperties.FirstOrDefault(y => y.CategoryId == request.CategoryId).CategoryId == request.CategoryId)
+                .ToListAsync();
+
+            return _mapper.Map<List<GetPropertyValueDTO>>(result);
+        }
+    }
+}
---
+++ OnlineShop.Application/CQRS/PropertyValues/Queries/GetPropertyValuesByCategoryId/GetPropertyValuesByCategoryIdQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using OnlineShop.Application.CQRS.PropertyValues.DTOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Application.CQRS.PropertyValues.Queries.GetPropertyValuesByCategoryId
+{
+    public class GetPropertyValuesByCategoryIdQuery : IRequest<List<GetPropertyValueDTO>>
+    {
+        [Required]
+        public int CategoryId { get; set; }
+    }
+}
