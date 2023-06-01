7c6443f586344de4b9ed1bc5da843d3f04a9301f
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-06-01 18:30:45 +0600

    Api: update Program.cs, update ConnectionString

--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -35,11 +35,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(c => 
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "OnlineShop");
+});
+
 
 app.UseHttpsRedirection();
 
