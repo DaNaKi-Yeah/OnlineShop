6e1a3229f0a09ec6a182134d21436d8a588fef69
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-06-02 19:07:14 +0600

    Application: update CreateReviewCommand

--- OnlineShop.Application/CQRS/Reviews/Commands/CreateReview/CreateReviewCommand.cs
+++ OnlineShop.Application/CQRS/Reviews/Commands/CreateReview/CreateReviewCommand.cs
@@ -7,7 +7,8 @@ namespace OnlineShop.Application.CQRS.Reviews.Commands.CreateReview
     public class CreateReviewCommand : IRequest<int>
     {
         public string Comment { get; set; }
-        public int Rating { get; set; }
+        [Range(0, 5, ErrorMessage = "Enter 0-5")]
+        public int Rating { get; set; } 
         [Required]
         public int ProductId { get; set; }
     }
--- OnlineShop.Application/CQRS/Reviews/Commands/CreateReview/CreateReviewCommandHandler.cs
+++ OnlineShop.Application/CQRS/Reviews/Commands/CreateReview/CreateReviewCommandHandler.cs
@@ -12,14 +12,12 @@ namespace OnlineShop.Application.CQRS.Reviews.Commands.CreateReview
 {
     public class CreateReviewCommandHandler : ReviewHandler, IRequestHandler<CreateReviewCommand, int>
     {
-        public CreateReviewCommandHandler(IRepository<Review, int> repository, IMapper mapper) : base(repository, mapper)
-        {
-        }
+        public CreateReviewCommandHandler(IRepository<Review, int> repository, IMapper mapper) : base(repository, mapper) { }
 
         public async Task<int> Handle(CreateReviewCommand request, CancellationToken cancellationToken)
         {
             Review review = new Review { Comment = request.Comment, Rating = request.Rating, ProductId = request.ProductId };
-
+            
             int id = await _repository.AddAsync(review);
 
             return id;
