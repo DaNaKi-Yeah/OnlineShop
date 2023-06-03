02c2d3dbd36258c5675149807edae1d80ecba398
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-06-03 20:15:28 +0600

    Application: update CreateOrderCommand

--- OnlineShop.Application/CQRS/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
+++ OnlineShop.Application/CQRS/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -10,15 +10,60 @@ namespace OnlineShop.Application.CQRS.Orders.Commands.CreateOrder
 {
     public class CreateOrderCommandHandler : OrderHandler, IRequestHandler<CreateOrderCommand, int>
     {
-        public CreateOrderCommandHandler(IRepository<Order, int> repository, IMapper mapper) : base(repository, mapper) { }
+        private readonly IRepository<Cart, int> _cartRepository;
+        private readonly IRepository<Payment, int> _paymentRepository;
+        public CreateOrderCommandHandler
+            (IRepository<Order, int> repository,
+            IRepository<Cart, int> cartRepository,
+            IRepository<Payment, int> paymentRepository,
+            IMapper mapper)
+            : base(repository, mapper)
+        {
+            _cartRepository = cartRepository;
+            _paymentRepository = paymentRepository;
+        }
 
         public async Task<int> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
         {
-            Order order = new Order { CartId = request.CartId, PaymentId = request.PaymentId };
+            var userId = request.UserId;
+            var cartOut = await _cartRepository.GetByIdAsync(request.CartId);
+            var buyItemIds = request.BuyItemIds;
+            var bankAccountId = request.BankAccountId;
 
-            int id = await _repository.AddAsync(order);
 
-            return id;
+            var buyItems = new List<BuyItem>();
+
+            foreach (var buyItem in cartOut.BuyItems)
+            {
+                if (buyItemIds.Contains(buyItem.Id))
+                {
+                    buyItems.Add(buyItem);
+                }
+            }
+
+            
+            var cart = new Cart() { UserId = userId, BuyItems = buyItems };
+            var cartId = await _cartRepository.AddAsync(cart);
+
+            
+            var payment = new Payment() { BankAccountId = bankAccountId };
+            var paymentId = await _paymentRepository.AddAsync(payment);
+
+
+            Order order = new Order();
+
+            order.CartId = cartId;
+            order.PaymentId = paymentId;
+
+            var orderId = await _repository.AddAsync(order);
+
+
+            payment = await _paymentRepository.GetByIdAsync(paymentId);
+            payment.OrderId = orderId;
+            await _paymentRepository.UpdateAsync(payment);
+
+
+            return orderId;
         }
     }
 }
