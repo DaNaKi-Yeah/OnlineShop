845121450c4b772f8f5748cb2b9da0c509d62051
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-06-03 20:16:32 +0600

    Application: add CQRS Users

---
+++ OnlineShop.Application/CQRS/Users/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using OnlineShop.Application.CQRS.Users.DTOs;
+using OnlineShop.Application.CQRS.Users.Handlers;
+using OnlineShop.Application.Repositories.Interfaces;
+using OnlineShop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Application.CQRS.Users.Commands.CreateUser
+{
+    public class CreateUserCommandHandler : UserHandler, IRequestHandler<CreateUserCommand, CreateUserDTO>
+    {
+        private readonly IRepository<Cart, int> _cartRepository;
+        public CreateUserCommandHandler(IRepository<User, int> repository, IRepository<Cart, int> cartRepository, IMapper mapper) : base(repository, mapper)
+        {
+            _cartRepository = cartRepository;
+        }
+
+        public async Task<CreateUserDTO> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            var userId = await _repository.AddAsync(new User());
+
+            var cartId = await _cartRepository.AddAsync(new Cart() {  UserId = userId});
+
+            var user = await _repository.GetByIdAsync(userId);
+
+            user.CartId = cartId;
+
+            await _repository.UpdateAsync(user);
+
+            return new CreateUserDTO { UserId = userId, CartId = cartId };
+        }
+    }
+}
---
+++ OnlineShop.Application/CQRS/Users/Queries/GetUsers/GetUsersQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using OnlineShop.Application.CQRS.Users.DTOs;
+using OnlineShop.Application.CQRS.Users.Handlers;
+using OnlineShop.Application.Repositories.Interfaces;
+using OnlineShop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Application.CQRS.Users.Queries.GetUsers
+{
+    public class GetUsersQueryHandler : UserHandler, IRequestHandler<GetUsersQuery, List<GetUserDTO>>
+    {
+        public GetUsersQueryHandler(IRepository<User, int> repository, IMapper mapper) : base(repository, mapper) { }
+
+        public async Task<List<GetUserDTO>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+        {
+            var list = await _repository.GetAllAsync();
+
+            return _mapper.Map<List<GetUserDTO>>(list);
+        }
+    }
+}
