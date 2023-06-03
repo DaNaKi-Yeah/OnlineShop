c0e36e796ac7973726e5b00bd597eac5e028968d
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-06-03 18:17:03 +0600

    Persistence: add migration updateUserAndCart

---
+++ OnlineShop.Persistence/Migrations/20230603121630_updateUserAndCart.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShop.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateUserAndCart : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BankAccounts_Clients_ClientId",
+                table: "BankAccounts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Clients_ClientId",
+                table: "Carts");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Carts_ClientId",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "Carts");
+
+            migrationBuilder.RenameColumn(
+                name: "ClientId",
+                table: "BankAccounts",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BankAccounts_ClientId",
+                table: "BankAccounts",
+                newName: "IX_BankAccounts_UserId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrderId",
+                table: "Carts",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Carts",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CartId = table.Column<int>(type: "int", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Carts_CartId",
+                        column: x => x.CartId,
+                        principalTable: "Carts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CartId",
+                table: "Users",
+                column: "CartId",
+                unique: true,
+                filter: "[CartId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BankAccounts_Users_UserId",
+                table: "BankAccounts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BankAccounts_Users_UserId",
+                table: "BankAccounts");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Carts");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "BankAccounts",
+                newName: "ClientId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BankAccounts_UserId",
+                table: "BankAccounts",
+                newName: "IX_BankAccounts_ClientId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrderId",
+                table: "Carts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ClientId",
+                table: "Carts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_ClientId",
+                table: "Carts",
+                column: "ClientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BankAccounts_Clients_ClientId",
+                table: "BankAccounts",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Clients_ClientId",
+                table: "Carts",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
