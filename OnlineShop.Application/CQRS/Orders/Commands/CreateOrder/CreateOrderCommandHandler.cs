727cfc1d7bf710893efd9ba97413763ee1958ec8
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-06-04 20:15:37 +0600

    Application: update CreateOrder

--- OnlineShop.Application/CQRS/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
+++ OnlineShop.Application/CQRS/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -12,15 +12,18 @@ namespace OnlineShop.Application.CQRS.Orders.Commands.CreateOrder
     {
         private readonly IRepository<Cart, int> _cartRepository;
         private readonly IRepository<Payment, int> _paymentRepository;
+        private readonly IRepository<BuyItem, int> _buyItemRepository;
         public CreateOrderCommandHandler
             (IRepository<Order, int> repository,
             IRepository<Cart, int> cartRepository,
             IRepository<Payment, int> paymentRepository,
+            IRepository<BuyItem, int> buyItemRepository,
             IMapper mapper)
             : base(repository, mapper)
         {
             _cartRepository = cartRepository;
             _paymentRepository = paymentRepository;
+            _buyItemRepository = buyItemRepository;
         }
 
         public async Task<int> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
@@ -31,7 +34,7 @@ namespace OnlineShop.Application.CQRS.Orders.Commands.CreateOrder
             var bankAccountId = request.BankAccountId;
 
 
-            var buyItems = new List<BuyItem>();
+            var buyItems = new List<BuyItem>(); //
 
             foreach (var buyItem in cartOut.BuyItems)
             {
@@ -41,11 +44,18 @@ namespace OnlineShop.Application.CQRS.Orders.Commands.CreateOrder
                 }
             }
 
-            
-            var cart = new Cart() { UserId = userId, BuyItems = buyItems };
+
+            var cart = new Cart() { UserId = userId };
             var cartId = await _cartRepository.AddAsync(cart);
 
-            
+
+            buyItems.ForEach(async x =>
+            {
+                x.CartId = cartId;
+                await _buyItemRepository.UpdateAsync(x);
+            });
+
+
             var payment = new Payment() { BankAccountId = bankAccountId };
             var paymentId = await _paymentRepository.AddAsync(payment);
 
