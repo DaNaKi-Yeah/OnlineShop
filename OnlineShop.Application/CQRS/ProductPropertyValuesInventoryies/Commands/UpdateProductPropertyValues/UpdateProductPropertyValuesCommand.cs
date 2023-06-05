d83f6c5241f6e90a96941006006713989d12c753
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-06-05 19:17:03 +0600

    Application: add UpdateProductPropertyValueInventory

---
+++ OnlineShop.Application/CQRS/ProductPropertyValuesInventoryies/Commands/UpdateProductPropertyValues/UpdateProductPropertyValuesCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Application.CQRS.ProductPropertyValuesInventoryies.Commands.UpdateProductPropertyValues
+{
+    public class UpdateProductPropertyValuesCommand : IRequest
+    {
+        [Required]
+        public int InventoryId { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ OnlineShop.Application/CQRS/ProductPropertyValuesInventoryies/Commands/UpdateProductPropertyValues/UpdateProductPropertyValuesCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using OnlineShop.Application.CQRS.ProductPropertyValuesInventoryies.Commands.CreateProductPropertyValuesInventory;
+using OnlineShop.Application.CQRS.ProductPropertyValuesInventoryies.Handlers;
+using OnlineShop.Application.Repositories.Interfaces;
+using OnlineShop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Application.CQRS.ProductPropertyValuesInventoryies.Commands.UpdateProductPropertyValues
+{
+    public class UpdateProductPropertyValuesCommandHandler :
+        ProductPropertyValuesInventoryHandler,
+        IRequestHandler<UpdateProductPropertyValuesCommand>
+    {
+        public UpdateProductPropertyValuesCommandHandler(IRepository<ProductPropertyValuesInventory, int> repository, IMapper mapper) : base(repository, mapper) { }
+
+        public async Task Handle(UpdateProductPropertyValuesCommand request, CancellationToken cancellationToken)
+        {
+            var inventory = await _repository.GetByIdAsync(request.InventoryId);
+
+            inventory.Count = request.Count;
+
+            await _repository.UpdateAsync(inventory);
+        }
+    }
+}
