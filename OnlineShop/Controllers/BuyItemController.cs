5d4c99aaf8dd42444cac6cbf3c3ccc3ce274252b
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-06-05 16:44:28 +0600

    Api: add BuyItemController

---
+++ OnlineShop/Controllers/BuyItemController.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Application.CQRS.BuyItems.Commands.CreateBuyItem;
+using OnlineShop.Application.CQRS.BuyItems.Commands.RemoveBuyItemById;
+using OnlineShop.Application.CQRS.BuyItems.Commands.UpdateBuyItem;
+using OnlineShop.Application.CQRS.BuyItems.DTOs;
+using OnlineShop.Application.CQRS.BuyItems.Queries.SearchBuyItems;
+using OnlineShop.Application.CQRS.BuyItems.Queries.SearchBuyItemsByUserId;
+
+namespace OnlineShop.API.Controllers
+{
+    public class BuyItemController : BaseController
+    {
+        public BuyItemController(IMediator mediator, IMapper mapper) : base(mediator, mapper) { }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<int> Create([FromBody] CreateBuyItemCommand command)
+        {
+            var id = await _mediator.Send(command);
+
+            return id;
+        }
+
+        [HttpPut]
+        [Route("Update")]
+        public async Task Update([FromBody] UpdateBuyItemCommand command)
+        {
+            await _mediator.Send(command);
+        }
+
+        [HttpDelete]
+        [Route("RemoveById")]
+        public async Task RemoveById([FromQuery] RemoveBuyItemByIdCommand command)
+        {
+            await _mediator.Send(command);
+        }
+
+        [HttpGet]
+        [Route("Search")]
+        public async Task<List<GetBuyItemDTO>> Search([FromQuery] SearchBuyItemsQuery query)
+        {
+            return await _mediator.Send(query);
+        }
+
+        [HttpGet]
+        [Route("SearchBuyItemsByUserId")]
+        public async Task<List<GetBuyItemDTO>> SearchBuyItemsByUserId([FromQuery] SearchBuyItemsByUserIdQuery query)
+        {
+            return await _mediator.Send(query);
+        }
+    }
+}
