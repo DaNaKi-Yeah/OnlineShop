7f3f0e21e65cebe74f4c446c2c5d8ab175f79bb8
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-06-06 18:59:45 +0600

    Domain: update User

--- OnlineShop.Domain/Models/User.cs
+++ OnlineShop.Domain/Models/User.cs
@@ -4,6 +4,7 @@ namespace OnlineShop.Domain.Models
 {
     public class User: BaseEntity<int>
     {
+        public string UserName { get; set; }
         public int? CartId { get; set; }
         public virtual Cart Cart { get; set; }
         public virtual List<BankAccount> BankAccounts { get; set; }
