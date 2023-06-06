e9ac23bc99c7046514febf42ebf91c4e6a2ec70d
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-06-06 19:02:40 +0600

    Application: update GetPaymentDTO, GetById, GetPsById

--- OnlineShop.Application/CQRS/Payments/Queries/SearchPaymentsByUserId/SearchPaymentsByUserIdQueryHandler.cs
+++ OnlineShop.Application/CQRS/Payments/Queries/SearchPaymentsByUserId/SearchPaymentsByUserIdQueryHandler.cs
@@ -14,25 +14,51 @@ namespace OnlineShop.Application.CQRS.Payments.Queries.SearchPaymentsByUserId
 {
     public class SearchPaymentsByUserIdQueryHandler : PaymentHandler, IRequestHandler<SearchPaymentsByUserIdQuery, List<GetPaymentDTO>>
     {
-        public SearchPaymentsByUserIdQueryHandler(IRepository<Payment, int> repository, IMapper mapper) : base(repository, mapper) { }
+        private readonly IRepository<User, int> _userRepository;
+
+        public SearchPaymentsByUserIdQueryHandler(IRepository<Payment, int> repository, IRepository<User, int> userRepository, IMapper mapper)
+            : base(repository, mapper)
+        {
+            _userRepository = userRepository;
+        }
 
         public async Task<List<GetPaymentDTO>> Handle(SearchPaymentsByUserIdQuery request, CancellationToken cancellationToken)
         {
             if (request == null || request.UserId == 0)
             {
-                return _mapper.Map<List<GetPaymentDTO>>(await _repository.GetAllAsync());
+                var allItems = await _repository.GetAllAsync();
+
+                var allGetItems = _mapper.Map<List<GetPaymentDTO>>(allItems);
+
+                foreach (var getItem in allGetItems)
+                {
+                    var cart = allItems.First(x => x.Id == getItem.Id).Order.Cart;
+                    var user = await _userRepository.GetByIdAsync((int)cart.UserId);
+                    getItem.UserName = user.UserName;
+                }
+
+                return allGetItems;
             }
 
 
-            var baseResult = _mapper.Map<List<GetPaymentDTO>>(await _repository.GetQuery().Where(x => x.Order.Cart.UserId == request.UserId)
-                .ToListAsync());
+            var items = await _repository.GetQuery().Where(x => x.Order.Cart.UserId == request.UserId).ToListAsync();
+
+            var getItems = _mapper.Map<List<GetPaymentDTO>>(items);
+
+            foreach (var getItem in getItems)
+            {
+                var cart = items.First(x => x.Id == getItem.Id).Order.Cart;
+                var user = await _userRepository.GetByIdAsync((int)cart.UserId);
+                getItem.UserName = user.UserName;
+            }
+
 
             if (request.PageSize == null || request.PageNumber == null)
             {
-                return baseResult;
+                return getItems;
             }
 
-            return baseResult
+            return getItems
                 .Skip((request.PageNumber.Value - 1) * request.PageSize.Value)
                 .Take(request.PageSize.Value).ToList();
         }
