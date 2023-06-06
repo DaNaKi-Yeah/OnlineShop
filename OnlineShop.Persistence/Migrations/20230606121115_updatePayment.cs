4eb1226a4afd376a903e8e0803a2ba90ece6186b
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-06-06 19:03:18 +0600

    Persistence: add migration updatePayment

---
+++ OnlineShop.Persistence/Migrations/20230606121115_updatePayment.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShop.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatePayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_OrderId",
+                table: "Payments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrderId",
+                table: "Payments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_OrderId",
+                table: "Payments",
+                column: "OrderId",
+                unique: true,
+                filter: "[OrderId] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_OrderId",
+                table: "Payments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrderId",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_OrderId",
+                table: "Payments",
+                column: "OrderId",
+                unique: true);
+        }
+    }
+}
