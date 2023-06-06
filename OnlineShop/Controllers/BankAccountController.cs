424e3471683b89f68003c2ea7ee6b9231a8b6eed
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-06-06 16:17:52 +0600

    Api: add BankAccountController

---
+++ OnlineShop/Controllers/BankAccountController.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Application.CQRS.BankAccounts.Commands.CreateBankAccount;
+using OnlineShop.Application.CQRS.BankAccounts.Commands.RemoveBankAccountById;
+using OnlineShop.Application.CQRS.BankAccounts.DTOs;
+using OnlineShop.Application.CQRS.BankAccounts.Queries.GetBankAccountById;
+using OnlineShop.Application.CQRS.BankAccounts.Queries.SearchBankAccountsByUserId;
+using OnlineShop.Application.CQRS.BuyItems.Commands.CreateBuyItem;
+using OnlineShop.Application.CQRS.BuyItems.Commands.RemoveBuyItemById;
+using OnlineShop.Application.CQRS.BuyItems.Commands.UpdateBuyItem;
+using OnlineShop.Application.CQRS.BuyItems.DTOs;
+using OnlineShop.Application.CQRS.BuyItems.Queries.GetBuyItemById;
+using OnlineShop.Application.CQRS.BuyItems.Queries.SearchBuyItems;
+using OnlineShop.Application.CQRS.BuyItems.Queries.SearchBuyItemsByUserId;
+
+namespace OnlineShop.API.Controllers
+{
+    public class BankAccountController : BaseController
+    {
+        public BankAccountController(IMediator mediator, IMapper mapper) : base(mediator, mapper) { }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<int> Create([FromBody] CreateBankAccountCommand command)
+        {
+            var id = await _mediator.Send(command);
+
+            return id;
+        }
+
+        [HttpDelete]
+        [Route("RemoveById")]
+        public async Task RemoveById([FromQuery] RemoveBankAccountByIdCommand command)
+        {
+            await _mediator.Send(command);
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        public async Task<GetBankAccountDTO> GetById([FromQuery] GetBankAccountByIdQuery query)
+        {
+            return await _mediator.Send(query);
+        }
+
+        [HttpGet]
+        [Route("SearchBankAccountsByUserId")]
+        public async Task<List<GetBankAccountDTO>> SearchBankAccountsByUserId([FromQuery] SearchBankAccountsByUserIdQuery query)
+        {
+            return await _mediator.Send(query);
+        }
+    }
+}
