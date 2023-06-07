5f4dc4d869378bf1df34311500f1cebde6d7a35f
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-06-07 20:26:52 +0600

    Api: update UserController

--- OnlineShop/Controllers/UserController.cs
+++ OnlineShop/Controllers/UserController.cs
@@ -3,7 +3,9 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using OnlineShop.Application.CQRS.Users.Commands.CreateUser;
 using OnlineShop.Application.CQRS.Users.DTOs;
-using OnlineShop.Application.CQRS.Users.Queries.GetUsers;
+using OnlineShop.Application.CQRS.Users.Queries.GetUsersByFullName;
+using OnlineShop.Application.CQRS.Users.Queries.GetUserByUserAccountId;
+using OnlineShop.Application.CQRS.Users.Queries.SearchUsers;
 
 namespace OnlineShop.API.Controllers
 {
@@ -19,8 +21,22 @@ namespace OnlineShop.API.Controllers
         }
 
         [HttpGet]
-        [Route("GetAll")]
-        public async Task<List<GetUserDTO>> GetAll([FromQuery] GetUsersQuery query)
+        [Route("GetUserByUserAccountId")]
+        public async Task<GetUserDTO> GetUserByUserAccountId([FromQuery] GetUserByUserAccountIdQuery query)
+        {
+            return await _mediator.Send(query);
+        }
+
+        [HttpGet]
+        [Route("GetUsersByFullName")]
+        public async Task<List<GetUserDTO>> GetUsersByFullName([FromQuery] GetUsersByFullNameQuery query)
+        {
+            return await _mediator.Send(query);
+        }
+
+        [HttpGet]
+        [Route("GetAllWithPagination")]
+        public async Task<List<GetUserDTO>> GetAllWithPagination([FromQuery] SearchUsersQuery query)
         {
             return await _mediator.Send(query);
         }
