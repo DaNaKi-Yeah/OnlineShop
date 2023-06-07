886cbd3874f503aa3d083a03dbb3260c29b03975
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-06-07 18:14:40 +0600

    Persistence: add migration updateUser_addUserAccountId

---
+++ OnlineShop.Persistence/Migrations/20230607121344_updateUser_addUserAccountId.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShop.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateUser_addUserAccountId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserAccountId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserAccountId",
+                table: "Users",
+                column: "UserAccountId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_AspNetUsers_UserAccountId",
+                table: "Users",
+                column: "UserAccountId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_AspNetUsers_UserAccountId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_UserAccountId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserAccountId",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
