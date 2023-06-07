2397eea61f5ba1e12a8bb5aaa7476e92a2e8f535
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-06-07 17:15:48 +0600

    Application: update SearchProductDTO

--- OnlineShop.Application/CQRS/Products/DTOs/SearchProductDTO.cs
+++ OnlineShop.Application/CQRS/Products/DTOs/SearchProductDTO.cs
@@ -8,6 +8,7 @@ namespace OnlineShop.Application.CQRS.Products.DTOs
     {
         public int Id { get; set; }
         public string ModelName { get; set; }
+        public int? YearOfProduction { get; set; }
         public string PictureLink { get; set; }
         public decimal Price { get; set; }
         public string AverageRating { get; set; } 
@@ -16,6 +17,7 @@ namespace OnlineShop.Application.CQRS.Products.DTOs
             profile.CreateMap<Product, SearchProductDTO>()
                 .ForMember(x => x.Id, opt => opt.MapFrom(y => y.Id))
                 .ForMember(x => x.ModelName, opt => opt.MapFrom(y => y.ModelName))
+                .ForMember(x => x.YearOfProduction, opt => opt.MapFrom(y => y.YearOfProduction))
                 .ForMember(x => x.PictureLink, opt => opt.MapFrom(y => y.PictureLink))
                 .ForMember(x => x.Price, opt => opt.MapFrom(y => y.Price))
                 .ForMember(x => x.AverageRating, opt => opt.MapFrom(
